9eacb4e07d374049e46bb46f58d7fa35efd57623
AnthonyJoelRY <ajromero12@utpl.edu.ec>
2026-05-01 09:26:59 -0500

    actualizar main y index para cambiar json,

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css';
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-dt/css/dataTables.dataTables.css';
 
-const API_URL = "https://raw.githubusercontent.com/PlataformasWeb-P-AA2026/api-demo/main/db.json"
+const API_URL = "https://gist.githubusercontent.com/emamut/6626d3dff58598b624a1/raw/166183f4520c4603987c55498df8d2983703c316/provincias.json"
 
 async function cargarDatos() {
   try {
@@ -12,15 +12,30 @@ async function cargarDatos() {
       throw new Error('Error al consumir la API');
     }
 
-    const datos = await respuesta.json();
+    const json = await respuesta.json();
+
+    // Transformar el objeto anidado en array plano
+    const datos = [];
+    for (const idProv in json) {
+      const prov = json[idProv];
+      for (const idCanton in prov.cantones) {
+        const canton = prov.cantones[idCanton];
+        for (const idParroquia in canton.parroquias) {
+          datos.push({
+            provincia: prov.provincia,
+            canton: canton.canton,
+            parroquia: canton.parroquias[idParroquia]
+          });
+        }
+      }
+    }
 
     new DataTable('#tabla-posts', {
       data: datos,
       columns: [
-        { data: 'AMIE' },
-        { data: 'Nombre-Educativa' },
-        { data: 'Canton' },
-        { data: 'Parroquia' }
+        { data: 'provincia' },
+        { data: 'canton' },
+        { data: 'parroquia' }
       ],
       pageLength: 10,
       language: {
